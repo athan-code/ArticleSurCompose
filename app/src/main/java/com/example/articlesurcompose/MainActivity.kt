b4e2d881fe77474b7bd2fc701a1519bce9000f22
Athan <athan.codes@gmail.com>
2023-04-29 23:25:00 +0200

    Exercice done with correction elements by athan-code 29/04/2023 23:20

--- app/src/main/java/com/example/articlesurcompose/MainActivity.kt
+++ app/src/main/java/com/example/articlesurcompose/MainActivity.kt
@@ -1,6 +1,4 @@
 package com.example.articlesurcompose
-
-import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -34,25 +33,37 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArticleWithImage(
-                        stringResource(R.string.article_title),
-                        stringResource(R.string.article_introduction),
-                        stringResource(R.string.article_corpus)
-                    )
+                    ComposeArticleApp()
                 }
             }
         }
     }
 }
 
+// Contenu
 @Composable
-fun ArticleWithImage(title: String, introduction: String, corps: String, modifier: Modifier = Modifier) {
+fun ComposeArticleApp(){
     // Chargement de la ressource : bg_(...).png
-    val image = painterResource(id = R.drawable.bg_compose_background)
+    ArticleWithImage(
+        image = painterResource(id = R.drawable.bg_compose_background),
+        title = stringResource(R.string.article_title),
+        introduction = stringResource(R.string.article_introduction),
+        corps = stringResource(R.string.article_corpus),
+    )
+}
 
+// Structure générique d'un article :
+@Composable
+fun ArticleWithImage(
+    image: Painter,
+    title: String,
+    introduction: String,
+    corps: String,
+    modifier: Modifier = Modifier
+) {
     // Disposition en colonne des éléments de l'écran
     Column (
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -66,22 +77,20 @@ fun ArticleWithImage(title: String, introduction: String, corps: String, modifie
             text = title,
             fontSize = 24.sp,
             modifier = Modifier
-                .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)
+                .padding(16.dp)
                 .align(alignment = Alignment.Start)
         )
         Text(
             text = introduction,
             textAlign = TextAlign.Justify,
-            fontSize = 16.sp,
             modifier = Modifier
                 .padding(start = 16.dp, end = 16.dp)
         )
         Text(
             text = corps,
             textAlign = TextAlign.Justify,
-            fontSize = 16.sp,
             modifier = Modifier
-                .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)
+                .padding(16.dp)
         )
     }
 }
@@ -90,10 +99,6 @@ fun ArticleWithImage(title: String, introduction: String, corps: String, modifie
 @Composable
 fun ArticleWithImagePreview() {
     ArticleSurComposeTheme {
-        ArticleWithImage(
-            stringResource(R.string.article_title),
-            stringResource(R.string.article_introduction),
-            stringResource(R.string.article_corpus)
-        )
+        ComposeArticleApp()
     }
 }
