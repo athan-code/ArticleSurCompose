9bf0429a01d943bdaecb9f34ad272a3661f361d6
Athan <athan.codes@gmail.com>
2023-04-29 23:11:33 +0200

    Exercice done by athan-code 29/04/2023 23:00

--- app/src/main/java/com/example/articlesurcompose/MainActivity.kt
+++ app/src/main/java/com/example/articlesurcompose/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.articlesurcompose
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.articlesurcompose.ui.theme.ArticleSurComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArticleWithImage(
+                        stringResource(R.string.article_title),
+                        stringResource(R.string.article_introduction),
+                        stringResource(R.string.article_corpus)
+                    )
                 }
             }
         }
@@ -30,17 +46,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArticleWithImage(title: String, introduction: String, corps: String, modifier: Modifier = Modifier) {
+    // Chargement de la ressource : bg_(...).png
+    val image = painterResource(id = R.drawable.bg_compose_background)
+
+    // Disposition en colonne des éléments de l'écran
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Background Image
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)
+                .align(alignment = Alignment.Start)
+        )
+        Text(
+            text = introduction,
+            textAlign = TextAlign.Justify,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = corps,
+            textAlign = TextAlign.Justify,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun ArticleWithImagePreview() {
     ArticleSurComposeTheme {
-        Greeting("Android")
+        ArticleWithImage(
+            stringResource(R.string.article_title),
+            stringResource(R.string.article_introduction),
+            stringResource(R.string.article_corpus)
+        )
     }
 }
